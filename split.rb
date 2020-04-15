321cb19157930eaeb9195307c1fdf0d24314a283
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-04-15 09:21:24 -0400

    Update split.rb
    Confirm palindrome

--- split.rb
+++ split.rb
@@ -1,39 +1,16 @@
-a = "A man, a plan, a canal, Panama".split(', ')
-# print a 
-# puts " "
-
-s = a.join
-# print s
-# puts " "
-# s = a
-# print s
-
-s = s.split
-# print s
-s = s.join
-# puts s
-
-
-def palindrome_tester(s, a)
-  if s == a.join.downcase
+def palindrome_tester(s)
+  if s == s.reverse
     puts "It's a palindrome!"
   else
     puts "It's not a palindrome."
   end
 end
 
-puts 'a is '
-print a
-puts ""
-puts 's is ' + s
-puts s
-
+a = "A man, a plan, a canal, Panama".split(', ')
+s = a.join
+s = s.split
+s = s.join
 s = s.downcase
-puts 'And now s is ' + s
 puts s
 
-palindrome_tester(s, a)
-
-# s = s.downcase
-# palindrome_tester(s, a)
-
+palindrome_tester(s)
