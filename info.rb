bae1834cf5ac6a6a170409ae655f90aa7141a15f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-04-15 12:02:11 -0400

    Update info.rb

--- info.rb
+++ info.rb
@@ -1,8 +1,3 @@
-Define a hash with symbol keys corresponding to name, 
-email, and a “password digest”, 
-and values equal to your name, your email address, 
-and a random string of 16 lower-case letters.
-
 info = {}
 info[:name] = "Phil"
 info[:email] = "phil@example.com"
