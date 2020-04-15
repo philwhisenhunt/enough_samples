d96cb44a15e63e10f0a3637ad8596b80911bea85
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-04-15 11:34:49 -0400

    Finish shuffler and start spanish counter

---
+++ shuffler.rb
@@ -0,0 +1,6 @@
+ def string_shuffle(s)
+   puts s.split('').shuffle.join
+ end
+
+string_shuffle("foobar")
+
---
+++ spanish_count.rb
@@ -0,0 +1,5 @@
+spanish_count = {'one' => 'uno', 'two' => 'dos', 'three' => 'tres'}
+
+spanish_count.each do |key, value|
+    puts "#{key} in Spanish is #{value}"
+end
