2d81be85240c76046f8c6d26071102e97bb9bbf0
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-04-15 11:56:29 -0400

    Create family.rb

---
+++ family.rb
@@ -0,0 +1,10 @@
+person1 = {:first => 'Bob', :last => "Iger"}
+person2 = {:first => 'Susan', :last => "Sans"}
+person3 = {:first => 'Larry', :last => "Liefeld"}
+
+params = {}
+params[:father] =   person1
+params[:mother] = person2
+params[:child] = person3
+
+puts params[:father][:first]
