57b3280fb54bfb532ad75eed9e104a26b81b8afa
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-04-16 09:31:27 -0400

    More examples and answers

--- info.rb
+++ info.rb
@@ -2,3 +2,5 @@ info = {}
 info[:name] = "Phil"
 info[:email] = "phil@example.com"
 info[:password_digest] = "aosdiasdaosdiasz"
+
+puts info
--- range.rb
+++ range.rb
@@ -1,3 +1,6 @@
-range = ('a'..'z').to_a
-puts range[7]
-puts range.reverse[7]
+# range = ('a'..'z').to_a
+# puts range[7]
+# puts range.reverse[7]
+
+
+puts (1..10) == Range.new(1,10)
